12421798ef33d65bb79f0f120f85de2a50cc8295
Misael Cabrera <misael.cabrera@encora.com>
2024-12-20 10:31:35 -0600

    [BT-1]: Product CRUD created

---
+++ backend/src/main/java/com/example/backend/repositories/ProductRepository.java
@@ -0,0 +1,40 @@
+package com.example.backend.repositories;
+
+import com.example.backend.models.Product;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class ProductRepository {
+
+    private static Map<Long, Product> products = new HashMap<>();
+    private static long currentId = 1L;
+
+    public Product save(Product product) {
+        product.setId(currentId++);
+        products.put(product.getId(), product);
+
+        return product;
+    }
+
+    public Optional<Product> getById(Long id) {
+        return Optional.ofNullable(products.get(id));
+    }
+
+    public Iterable<Product> getAll() {
+        return products.values();
+    }
+
+    public boolean delete(Long id) {
+        return products.remove(id) != null;
+    }
+
+    public Product update(Long id, Product product) {
+        if (products.containsKey(id)) {
+            product.setId(id);
+            products.put(id, product);
+            return product;
+        }
+        return null;
+    }
+}
